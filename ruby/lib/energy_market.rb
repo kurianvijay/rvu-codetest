56c635d27092736cf32302f408cda4b847da0d96
Vijay Kurian <kurianvijay7@gmail.com>
2020-04-07 21:32:28 +0100

    Add Plan class with all available plans, Add method to return cost when variable plan is applied

--- ruby/lib/energy_market.rb
+++ ruby/lib/energy_market.rb
@@ -5,6 +5,24 @@ class EnergyMarket
     @plans = plans
   end
 
+# Returns cost for the variable plan
+  def variable(con)
+  	if con <= 100
+      price = con * 13.5
+      vat = price * 0.05
+      full_price = price + vat
+      p vat_price = full_price / 100
+  	elsif con > 100
+      s = 13.5 * 100
+      con = con - 100
+      full_price = s + (con * 10)
+      vat = full_price * 0.05
+      vat_price = (full_price + vat)/100
+    end
+  end
+
+
+
   # Returns an array of plans available in the market for a given consumption.
   #
   # [{"supplier"=>"edf", "plan name"=>"fixed", "price"=>"205.75"},
---
+++ ruby/spec/energy_market_spec.rb
@@ -0,0 +1,1 @@
+require 'energy_market'
